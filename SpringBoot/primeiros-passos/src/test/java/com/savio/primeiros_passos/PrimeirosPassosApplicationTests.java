b87bbc8e94c4620097a3bb8262a1f221cdcf37eb
Sávio Soares Sousa <ssoaressousas@gmail.com>
2025-05-06 06:56:04 -0300

    Inicialização do projeto de Web API com SpringBoot

---
+++ SpringBoot/primeiros-passos/src/test/java/com/savio/primeiros_passos/PrimeirosPassosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savio.primeiros_passos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeirosPassosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
